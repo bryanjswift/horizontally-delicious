0c79418dd68aec97c84dbdcfc2fa060ac83c2626
bryanjswift <bryanjswift@bmac.local>
2008-05-11 00:23:54 -0400

    added a message about using git hub in the footer

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
 			</div> <!-- end of div.body -->
 			<div class="footer">
 				<p>Powered by <a href="http://wordpress.org/" title="Wordpress">Wordpress</a></p>
+				<!-- <p>Development aided by <a href="http://www.github.com/bryanjswift" title="bryanjswift's GitHub profile">GitHub</a></p> -->
 			</div>
 		</div> <!-- end of div.wrap -->
 	</body>
