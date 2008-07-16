c60280c8ad540fa5a98ddb6e6efdb08478c3cc4e
Bryan J Swift <bryan.j.swift@gmail.com>
2008-07-16 17:16:29 -0400

    updated the way the js works so a class is being added rather than toggling display

--- js/hd.js
+++ js/hd.js
@@ -2,9 +2,9 @@ var jq = jQuery.noConflict();
 jq.browser.ver = parseInt(jq.browser.version.split('.')[0]);
 if (jq.browser.msie && jq.browser.ver === 6) {
 	jq('#nav li.parent').bind('mouseenter',function(e) {
-		jq('ul.child',e.target).css('display','block');
+		jq(e.target).addClass('hovered');
 	});
 	jq('#nav li.parent').bind('mouseleave',function(e) {
-		jq('ul.child',e.target).css('display','none');
+		jq(e.target).removeClasse('hovered');
 	});
 }
