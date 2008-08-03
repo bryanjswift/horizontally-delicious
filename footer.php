0c477ce3fc16df4f3b56c249d197e00178f0b544
Bryan J Swift <bryan@bryanjswift.com>
2008-08-03 15:36:33 -0400

    added type attribute to script elements

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 			<p>Crafted by <a href="http://bryanjswift.com" title="Post-Swiftalism">Bryan J Swift</a> | Powered by <a href="http://wordpress.org/" title="Wordpress">Wordpress</a></p>
 			<!-- <p>Development aided by <a href="http://www.github.com/bryanjswift" title="bryanjswift's GitHub profile">GitHub</a></p> -->
 		</div>
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
-		<script src="<?php bloginfo('template_url'); ?>/js/hd.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js" type="text/javascript"></script>
+		<script src="<?php bloginfo('template_url'); ?>/js/hd.js" type="text/javascript"></script>
 	</body>
 </html>
