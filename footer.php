a84db5ee51e92c5cf7dc46a6f661bb81bc5c2ad0
Bryan J Swift <bryan.j.swift@gmail.com>
2009-01-25 17:37:21 -0500

    removed tacky 'crafted by' from the footer

--- footer.php
+++ footer.php
@@ -2,8 +2,7 @@
 		</div> <!-- end of div.body -->
 		<?php get_sidebar(); ?>
 		<div id="footer" class="wrap">
-			<p>Crafted by <a href="http://bryanjswift.com" title="Post-Swiftalism">Bryan J Swift</a> | Powered by <a href="http://wordpress.org/" title="Wordpress">Wordpress</a></p>
-			<!-- <p>Development aided by <a href="http://www.github.com/bryanjswift" title="bryanjswift's GitHub profile">GitHub</a></p> -->
+			<p>Powered by <a href="http://wordpress.org/" title="Wordpress">Wordpress</a></p>
 		</div>
 		<script type="text/javascript"> var stylesheetDirectory = '<?php bloginfo('template_directory'); ?>'; </script>
 		<?php $themeOptions = get_option('Horizontally Delicious'); ?>
